d83365e0a0d507ac9eefc92c9911165f3faa22c2
NataliaVainstein <Natalia.Vainstein@outlook.com>
2025-02-22 12:38:17 -0500

    add the correct solution of 3Sum question

--- submissions/3Sum.cpp
+++ submissions/3Sum.cpp
@@ -2,26 +2,37 @@
 
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        size_t numsLength = nums.size();
 
-        if (numsLength == 0)
-            return 0;
-        if (numsLength == 1)
-            return nums[0];
+    void setNextStartIdx(vector<int>& nums, size_t& startIdx, size_t n) {
+        ++startIdx;
 
-        int sum = 0;
-        int maxSum = nums[0];
+        while ((startIdx < n) && (nums[startIdx - 1] == nums[startIdx]))
+            ++startIdx;
+    }
 
-        for (int num : nums) {
-            if (sum <= 0)
-                sum = num;
-            else
-                sum += num;
-            if (sum > maxSum)
-                maxSum = sum;
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> result;
+        size_t n = nums.size();
+        for (size_t runningIdx = 0; runningIdx < n - 2; ++runningIdx) {
+            if ((runningIdx == 0) || (nums[runningIdx - 1] != nums[runningIdx])) {
+                size_t startIdx = runningIdx + 1;
+                size_t endIdx = n - 1;
+                while (startIdx < endIdx) {
+                    int resSum = nums[runningIdx] + nums[startIdx] + nums[endIdx];
+                    if (resSum == 0) {
+                        result.push_back({ nums[runningIdx], nums[startIdx], nums[endIdx] });
+                        setNextStartIdx(nums, startIdx, n);
+                    }
+                    else if (resSum < 0)
+                        ++startIdx;
+                    else
+                        --endIdx;
+                }
+            }
         }
 
-        return maxSum;
+        return result;
     }
 };
